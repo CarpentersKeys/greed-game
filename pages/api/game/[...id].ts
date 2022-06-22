ff2dd9adf33bd65f2b1e367ff4a14664c4278fdf
joshua <carpenterskeys@gmail.com>
2022-06-22 15:13:18 -0400

    Fixed: remove invald games, just use javascript and finByIdAndDelete now

--- pages/api/game/[...id].ts
+++ pages/api/game/[...id].ts
@@ -7,7 +7,6 @@ import { IGame } from "../../../models/game/types";
 import { isGame, isObjectId, ObjectId } from "../../../models/typeCheckers";
 import { TObjectId } from "../../../models/typeCheckers";
 import { STATE_QUERY, REMOVE_PLAYER_FROM_GAME, JOIN_OR_CREATE_GAME, GET_ALL_QUERY } from "../../../lib/famousStrings";
-import { playerSchema } from "../../../models/player/mongoose";
 //TODO method to prevent simultaneous joins resulting in more than 2 players in the game
 //TODO when ending session kill all open games
 export default async function (
@@ -19,7 +18,7 @@ export default async function (
         | { [GET_ALL_QUERY]: IGame[] }
         // useMutateGame resps
         | { [JOIN_OR_CREATE_GAME]: TObjectId }
-        | { [REMOVE_PLAYER_FROM_GAME]: { updatedGameIds: TObjectId[], deletedGames: object } }
+        | { [REMOVE_PLAYER_FROM_GAME]: { updatedGameIds: TObjectId[], deletedGameIds: TObjectId[] } }
     >
 ) {
     await dbConnect();
@@ -30,12 +29,12 @@ export default async function (
         // queries
         case STATE_QUERY:
             {
-                const endPointErrorResp = pathBadResp({ endPoint: STATE_QUERY });
-                if (endPointErrorResp({ evaluator: isObjectId, value: postData })) { return; };
+                const endPointBadResp = pathBadResp({ endPoint: STATE_QUERY });
+                if (endPointBadResp({ evaluator: isObjectId, value: postData })) { return; };
                 const gameId = postData;
                 const gameState =
                     await Game.findById(gameId);
-                if (endPointErrorResp({ evaluator: isGame, value: gameState })) {
+                if (endPointBadResp({ evaluator: isGame, value: gameState })) {
                     return;
                 };
                 return resp.status(200).json({ [STATE_QUERY]: gameState });
@@ -44,11 +43,11 @@ export default async function (
 
         case GET_ALL_QUERY:
             {
-                const endPointErrorResp = pathBadResp({ endPoint: GET_ALL_QUERY });
+                const endPointBadResp = pathBadResp({ endPoint: GET_ALL_QUERY });
                 const games =
                     await Game.find().sort({ updatedAt: -1 });
 
-                if (endPointErrorResp({
+                if (endPointBadResp({
                     evaluator(games: unknown) {
                         if (!Array.isArray(games)) { return; };
                         return (games.every(g => isGame(g)))
@@ -60,8 +59,8 @@ export default async function (
         // mutations
         case JOIN_OR_CREATE_GAME:
             {
-                const endPointErrorResp = pathBadResp({ endPoint: JOIN_OR_CREATE_GAME });
-                if (endPointErrorResp({ evaluator: isObjectId, value: postData })) { return; };
+                const endPointBadResp = pathBadResp({ endPoint: JOIN_OR_CREATE_GAME });
+                if (endPointBadResp({ evaluator: isObjectId, value: postData })) { return; };
                 const playerId = postData;
                 // check for open game
                 const openGame = await findOpenGame(); // defined below
@@ -69,30 +68,33 @@ export default async function (
                     ? playerGameAction(playerId, openGame, EPlayerGameAction.JOIN)
                     : new Game({ players: [playerId], isOpen: true });
                 const savedGame = await closedGame.save();
-                if (endPointErrorResp({ evaluator: isObjectId, value: savedGame._id })) { return; };
+                if (endPointBadResp({ evaluator: isObjectId, value: savedGame._id })) { return; };
                 return resp.status(200).json({ [JOIN_OR_CREATE_GAME]: savedGame._id });
             }
             break;
 
         case REMOVE_PLAYER_FROM_GAME:
-            if (pathBadResp({ evaluator: isObjectId, value: postData, endPoint: JOIN_OR_CREATE_GAME })) { return; };
+            const endPointBadResp = pathBadResp({ endPoint: JOIN_OR_CREATE_GAME });
+            if (endPointBadResp({ evaluator: isObjectId, value: postData })) { return; };
             const playerId = postData;
             // remove player from all games
             // TODO end sesesion doesn't removed playes from array
             const games = await Game.find({ players: { $in: playerId } });
             const updatedGameIds = [];
+            const deletedGameIds = [];
             for (const g of games) {
                 const updated = await playerGameAction(playerId, g, EPlayerGameAction.REMOVE).save();
-                updatedGameIds.push(updated._id);
+                if (endPointBadResp({ evaluator: '!', value: updated })) { return; };
+                updatedGameIds.push(g._id);
+                const pLength = g.players.length;
+                const isValid = pLength === 1 || pLength === 2
+                if (!isValid) {
+                    const deleted = await Game.findByIdAndDelete(g._id);
+                    if (endPointBadResp({ evaluator: '!', value: deleted.acknowledged })) { return; };
+                    deletedGameIds.push(g._id);
+                }
             }
-            const deletedGames = await Game.deleteMany({
-                $or: [
-                    { $expr: { $lt: [{ $size: '$players' }, 1], } },
-                    { $exists: 'players.2' }
-                ]
-            })
-            console.log(deletedGames);
-            return resp.status(200).json({ [REMOVE_PLAYER_FROM_GAME]: { updatedGameIds, deletedGames } });
+            return resp.status(200).json({ [REMOVE_PLAYER_FROM_GAME]: { updatedGameIds, deletedGameIds } });
             break;
 
         default:
